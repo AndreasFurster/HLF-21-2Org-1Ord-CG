f3adcd7ceec92b9735ade862e0e88e8e69356aa2
Andreas Furster <andreas.furster@hotmail.com>
2020-10-29 20:21:41 +0000

    Only delete generated files in stop.sh instead of all untracked files

--- stop.sh
+++ stop.sh
@@ -8,14 +8,5 @@ IMAGE_TAG=$IMAGETAG
 printSeparator "Shutdown Docker containers, remove volumes and orphans"
 docker-compose ${COMPOSE_FILES} down --volumes --remove-orphans
 
-printSeparator "Untracked files:"
-git clean -xdn 
-
-printSeparator "Are you sure you want to delete all files above (y/n)?"
-
-read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-  git clean -xdf
-else
-  echo "Skipped deleting untracked files."
-fi
+printSeparator "Delete generated files"
+rm -r ./generated/*
